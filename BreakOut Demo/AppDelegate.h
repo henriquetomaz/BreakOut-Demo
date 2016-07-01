26c95403b8d078fca8092baafeb4d827968a0286
Sebastián Badea <sebastian.badea@infojobs.net>
2016-07-01 07:22:04 +0200

    Initial Commit

---
+++ BreakOut Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BreakOut Demo
+//
+//  Created by Sebastián Badea on 1/7/16.
+//  Copyright © 2016 Sebastian Badea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BreakOut Demo/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  BreakOut Demo
+//
+
+//  Copyright (c) 2016 Sebastian Badea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
