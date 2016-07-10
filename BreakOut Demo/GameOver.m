feb9597d8166b15b0b2d0208df9cb8c6335ea770
Sebastián Badea <sebastian.badea@infojobs.net>
2016-07-11 01:03:56 +0200

    Added game over scene classes

---
+++ BreakOut Demo/GameOver.m
@@ -0,0 +1,13 @@
+//
+//  GameOver.m
+//  BreakOut Demo
+//
+//  Created by Sebastián Badea on 11/7/16.
+//  Copyright © 2016 Sebastian Badea. All rights reserved.
+//
+
+#import "GameOver.h"
+
+@implementation GameOver
+
+@end
