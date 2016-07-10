e99d628f88f0e4245f8775cb7227537310977507
Sebastián Badea <sebastian.badea@infojobs.net>
2016-07-11 01:03:09 +0200

    Added game start scene classes

---
+++ BreakOut Demo/GameStart.m
@@ -0,0 +1,13 @@
+//
+//  GameStart.m
+//  BreakOut Demo
+//
+//  Created by Sebastián Badea on 11/7/16.
+//  Copyright © 2016 Sebastian Badea. All rights reserved.
+//
+
+#import "GameStart.h"
+
+@implementation GameStart
+
+@end
