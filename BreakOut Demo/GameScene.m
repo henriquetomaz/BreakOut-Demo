7fed3feb9e3fac6c864a4dd47a91d09ba71e8fd8
Sebastián Badea <sebastian.badea@infojobs.net>
2016-07-27 07:44:16 +0200

    Updated game scene to set the root node category, collision and contact test bit masks

--- BreakOut Demo/GameScene.m
+++ BreakOut Demo/GameScene.m
@@ -32,6 +32,9 @@ static const uint32_t category_ball     = 0x1 < 0; // 0x000000000000000000000000
     // set scene (aka root SKNode) physics body borders as the scene edges
     self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
     
+    self.physicsBody.categoryBitMask = category_fence;
+    self.physicsBody.collisionBitMask = 0x0; // nothing colligding with the fence should move the fence
+    self.physicsBody.contactTestBitMask = 0x0; // no callback when something hits the fence
     SKSpriteNode *ball1 = [SKSpriteNode spriteNodeWithImageNamed:@"Blue Ball.png"];
     ball1.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball1.size.width/2];
     ball1.physicsBody.dynamic = YES;
