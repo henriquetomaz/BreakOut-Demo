532ed72261832ca9baf38fdb3c42363643c7f80c
Sebastián Badea <sebastian.badea@infojobs.net>
2016-08-05 16:07:02 +0200

    Added game won scene

---
+++ BreakOut Demo/GameWon.h
@@ -0,0 +1,13 @@
+//
+//  GameWon.h
+//  BreakOut Demo
+//
+//  Created by Sebastián Badea on 5/8/16.
+//  Copyright © 2016 Sebastian Badea. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameWon : SKScene
+
+@end
---
+++ BreakOut Demo/GameWon.m
@@ -0,0 +1,13 @@
+//
+//  GameWon.m
+//  BreakOut Demo
+//
+//  Created by Sebastián Badea on 5/8/16.
+//  Copyright © 2016 Sebastian Badea. All rights reserved.
+//
+
+#import "GameWon.h"
+
+@implementation GameWon
+
+@end
