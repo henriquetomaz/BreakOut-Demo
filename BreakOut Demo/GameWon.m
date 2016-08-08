53a0b702b7bea22a85581d8183fb257ae53017e5
Sebastián Badea <sebastian.badea@infojobs.net>
2016-08-08 07:24:00 +0200

    Updated game won scene to add touched began implementation: restart game

--- BreakOut Demo/GameWon.m
+++ BreakOut Demo/GameWon.m
@@ -7,7 +7,24 @@
 //
 
 #import "GameWon.h"
+#import "GameScene.h"
 
 @implementation GameWon
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    /* Called when a touch begins */
+    
+    if (touches) {
+        // Configure the view.
+        SKView * skView = (SKView *)self.view;
+        
+        // Create and configure the scene.
+        GameScene *scene = [GameScene nodeWithFileNamed:@"GameScene"];
+        scene.scaleMode = SKSceneScaleModeAspectFit;
+        
+        // Present the scene.
+        [skView presentScene:scene];
+    }
+}
 
 @end
